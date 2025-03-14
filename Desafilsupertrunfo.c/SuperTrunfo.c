4dd6b49df581e7657ee97c4a33f89e865dd921f0
Robe2712 <soare.jipa12@gmail.com>
2025-03-14 01:23:06 +0000

    adicionando os char

--- Desafilsupertrunfo.c/SuperTrunfo.c
+++ Desafilsupertrunfo.c/SuperTrunfo.c
@@ -3,6 +3,9 @@
 int main(){
     printf("Carta Super Trunfo \n");
     
-    
+    char Estado [50]="Amazonas";
+    char Codigo_da_Carta [3]= "A1";
+    char Nome_Da_Cidade [50]= "Manaus";
+
     return 0; 
 }
